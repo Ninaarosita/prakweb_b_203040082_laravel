6a3cb5f0802cd5630e516a5140a482f074b5407d
Ninaarosita <nina.203040082@mail.unpas.ac.id>
2022-10-21 17:12:32 +0700

    video 3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Home');
+});
+
+Route::get('/about', function () {
+    return view('About', [
+        "name" => "Nina Rosita",
+        "email" => "ninaarosita25@gmail.com",
+        "image" => "nina.jpg",
+
+    ]);
+});
+
+
+Route::get('/blog', function () {
+    return view('posts');
 });
