a43d9325811b0f9f19b476d6d5fb237cf59e40aa
Ninaarosita <nina.203040082@mail.unpas.ac.id>
2022-10-31 12:20:30 +0700

    vidio 9

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,67 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Nina Rosita',
+            'email' => 'ninaarosita25@gmail.com',
+            'password' => bcrypt('12345')
+
+        ]);
+
+        User::create([
+            'name' => 'Siti',
+            'email' => 'sitinurhasanah@gmail.com',
+            'password' => bcrypt('12345')
+
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates ullam eius deserunt laborum voluptas ipsum earum itaque harum sunt, explicabo a cupiditate optio asperiores, ea accusamus dolores quae consequatur dolor vel voluptatibus velit necessitatibus reiciendis totam commodi? Doloremque magnam harum accusamus, reiciendis nulla eligendi laborum iusto modi repudiandae.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates ullam eius deserunt laborum voluptas ipsum earum itaque harum sunt, explicabo a cupiditate optio asperiores, ea accusamus dolores quae consequatur dolor vel voluptatibus velit necessitatibus reiciendis totam commodi? Doloremque magnam harum accusamus, reiciendis nulla eligendi laborum iusto modi repudiandae.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates ullam eius deserunt laborum voluptas ipsum earum itaque harum sunt, explicabo a cupiditate optio asperiores, ea accusamus dolores quae consequatur dolor vel voluptatibus velit necessitatibus reiciendis totam commodi? Doloremque magnam harum accusamus, reiciendis nulla eligendi laborum iusto modi repudiandae.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates ullam eius deserunt laborum voluptas ipsum earum itaque harum sunt, explicabo a cupiditate optio asperiores, ea accusamus dolores quae consequatur dolor vel voluptatibus velit necessitatibus reiciendis totam commodi? Doloremque magnam harum accusamus, reiciendis nulla eligendi laborum iusto modi repudiandae.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,8 +2,9 @@
 
 @section('container')
 <h2>{{ $post->title }}</h2>
-<p>By. Nina Rosita in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name   }}</a></p></p>
+
+<p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 {!! $post->body!!}
-<a href="/posts">Back to Posts</a>
+<a href="/posts" class="d-block mt-3">Back to Posts</a>
     
 @endsection
