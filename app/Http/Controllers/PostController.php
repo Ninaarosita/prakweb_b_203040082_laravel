cf43af7b1042997df35466793b018ec884e3559b
Ninaarosita <nina.203040082@mail.unpas.ac.id>
2022-10-31 13:17:29 +0700

    video 10

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,8 @@ class PostController extends Controller
     {
         return view('posts', [
             "title" => "Posts",
-            "posts" => Post::all()
+            // "posts" => post::all()
+            "posts" => post::latest()->get()
         ]);
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,73 +17,69 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // User::create([
+        //     'name' => 'Nina Rosita',
+        //     'email' => 'ninaarosita25@gmail.com',
+        //     'password' => bcrypt('12345')
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
         // ]);
 
-        User::create([
-            'name' => 'Nina Rosita',
-            'email' => 'ninaarosita25@gmail.com',
-            'password' => bcrypt('12345')
+        // User::create([
+        //     'name' => 'Siti',
+        //     'email' => 'sitinurhasanah@gmail.com',
+        //     'password' => bcrypt('12345')
 
-        ]);
+        // ]);
 
-        User::create([
-            'name' => 'Siti',
-            'email' => 'sitinurhasanah@gmail.com',
-            'password' => bcrypt('12345')
-
-        ]);
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programming',
-            'slug' => 'web-programming'
+             'slug' => 'web-programming'
         ]);
 
         Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+             'name' => 'Personal',
+             'slug' => 'personal'
         ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates ullam eius deserunt laborum voluptas ipsum earum itaque harum sunt, explicabo a cupiditate optio asperiores, ea accusamus dolores quae consequatur dolor vel voluptatibus velit necessitatibus reiciendis totam commodi? Doloremque magnam harum accusamus, reiciendis nulla eligendi laborum iusto modi repudiandae.',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
-
-
-
-        Post::create([
-            'title' => 'Judul Ke Dua',
-            'slug' => 'judul-ke-dua',
-            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates ullam eius deserunt laborum voluptas ipsum earum itaque harum sunt, explicabo a cupiditate optio asperiores, ea accusamus dolores quae consequatur dolor vel voluptatibus velit necessitatibus reiciendis totam commodi? Doloremque magnam harum accusamus, reiciendis nulla eligendi laborum iusto modi repudiandae.',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
-        Post::create([
-            'title' => 'Judul Ke Tiga',
-            'slug' => 'judul-ke-tiga',
-            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates ullam eius deserunt laborum voluptas ipsum earum itaque harum sunt, explicabo a cupiditate optio asperiores, ea accusamus dolores quae consequatur dolor vel voluptatibus velit necessitatibus reiciendis totam commodi? Doloremque magnam harum accusamus, reiciendis nulla eligendi laborum iusto modi repudiandae.',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        //     'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates ullam eius deserunt laborum voluptas ipsum earum itaque harum sunt, explicabo a cupiditate optio asperiores, ea accusamus dolores quae consequatur dolor vel voluptatibus velit necessitatibus reiciendis totam commodi? Doloremque magnam harum accusamus, reiciendis nulla eligendi laborum iusto modi repudiandae.',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
 
-        Post::create([
-            'title' => 'Judul Ke Empat',
-            'slug' => 'judul-ke-empat',
-            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates ullam eius deserunt laborum voluptas ipsum earum itaque harum sunt, explicabo a cupiditate optio asperiores, ea accusamus dolores quae consequatur dolor vel voluptatibus velit necessitatibus reiciendis totam commodi? Doloremque magnam harum accusamus, reiciendis nulla eligendi laborum iusto modi repudiandae.',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ke Dua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        //     'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates ullam eius deserunt laborum voluptas ipsum earum itaque harum sunt, explicabo a cupiditate optio asperiores, ea accusamus dolores quae consequatur dolor vel voluptatibus velit necessitatibus reiciendis totam commodi? Doloremque magnam harum accusamus, reiciendis nulla eligendi laborum iusto modi repudiandae.',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Tiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        //     'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates ullam eius deserunt laborum voluptas ipsum earum itaque harum sunt, explicabo a cupiditate optio asperiores, ea accusamus dolores quae consequatur dolor vel voluptatibus velit necessitatibus reiciendis totam commodi? Doloremque magnam harum accusamus, reiciendis nulla eligendi laborum iusto modi repudiandae.',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
+
+
+        // Post::create([
+        //     'title' => 'Judul Ke Empat',
+        //     'slug' => 'judul-ke-empat',
+        //     'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        //     'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates ullam eius deserunt laborum voluptas ipsum earum itaque harum sunt, explicabo a cupiditate optio asperiores, ea accusamus dolores quae consequatur dolor vel voluptatibus velit necessitatibus reiciendis totam commodi? Doloremque magnam harum accusamus, reiciendis nulla eligendi laborum iusto modi repudiandae.',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
     }
 }
